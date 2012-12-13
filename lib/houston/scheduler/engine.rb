1dd4211607a23259ad3947c3972e97889710e500
Robert Lail <robert.lail@cph.org>
2012-12-13 07:52:53 -0600

    precompile assets

--- lib/houston/scheduler/engine.rb
+++ lib/houston/scheduler/engine.rb
@@ -2,6 +2,14 @@ module Houston
   module Scheduler
     class Engine < ::Rails::Engine
       isolate_namespace Houston::Scheduler
+      
+      # Enabling assets precompiling under rails 3.1
+      if Rails.version >= '3.1'
+        initializer :assets do |config|
+          Rails.application.config.assets.precompile += %w( houston-scheduler/application.js houston-scheduler/application.css )
+        end
+      end
+      
     end
   end
 end
