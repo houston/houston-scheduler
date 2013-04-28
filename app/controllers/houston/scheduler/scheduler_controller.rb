6f7bb74f0a058c5da495c91afb2ed05c78612898
Robert Lail <robert.lail@cph.org>
2013-04-28 14:31:29 -0500

    [experiment] show the projects current velocity in the Priority view

--- app/controllers/houston/scheduler/scheduler_controller.rb
+++ app/controllers/houston/scheduler/scheduler_controller.rb
@@ -12,6 +12,11 @@ module Houston
       
       def project
         @project = Project.find_by_slug!(params[:slug])
+        velocity = Setting["Velocity"].to_i
+        quota = ProjectQuota.where(project_id: @project.id).order(:week).where(["week <= ?", Date.today]).first
+        value = quota ? quota.value : 0
+        @velocity = (velocity * (value / 100.0)).round(1)
+        
         if @project.has_ticket_tracker?
           @tickets = @project.find_tickets(status: neq(:closed), resolution: 0)
         else
