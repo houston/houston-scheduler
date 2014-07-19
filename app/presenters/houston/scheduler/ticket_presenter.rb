ab508dbd5e895d7a2cfa07b9fbbee58b0ce28355
Bob Lail <bob.lail@cph.org>
2014-07-18 21:03:08 -0500

    [improvement] Stopped rendering version history for _every_ ticket (4m)

--- app/presenters/houston/scheduler/ticket_presenter.rb
+++ app/presenters/houston/scheduler/ticket_presenter.rb
@@ -1,7 +1,19 @@
-class Houston::Scheduler::TicketPresenter < FullTicketPresenter
+class Houston::Scheduler::TicketPresenter < TicketPresenter
+  include MarkdownHelper
   
   def ticket_to_json(ticket)
+    reporter = ticket.reporter
     super.merge(
+      description: mdown(ticket.description),
+      tasks: ticket.tasks.map { |task| task.ticket = ticket; {
+        id: task.id,
+        description: task.description,
+        number: task.number,
+        letter: task.letter,
+        effort: task.effort } },
+      reporter: reporter && {
+        email: reporter.email,
+        name: reporter.name },
       milestoneId: ticket.milestone_id,
       milestoneName: ticket.milestone.try(:name),
       sprintId: ticket.sprint_id,
