0e29c6671c952b306768fae7e7cb7ed764c5b743
Bob Lail <bob.lail@cph.org>
2015-10-10 11:24:35 -0500

    [skip] Namespaced routes (1m)

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Houston::Scheduler::Engine.routes.draw do
 
-  get "mixer", :to => "mixer#index", :as => :mixer
-  put "mixer", :to => "mixer#update"
+  scope "scheduler" do
+    get "mixer", :to => "mixer#index", :as => :mixer
+    put "mixer", :to => "mixer#update"
 
-  get "by_project/:slug", :to => "scheduler#project", :as => :project
-  put "by_project/:slug/ticket_order", :to => "tickets#update_order"
+    get "by_project/:slug", :to => "scheduler#project", :as => :project
+    put "by_project/:slug/ticket_order", :to => "tickets#update_order"
 
-  put "projects/:id/value_statements", :to => "value_statements#update"
+    put "projects/:id/value_statements", :to => "value_statements#update"
 
-  put "tickets/:id", :to => "tickets#update", constraints: {id: /\d+/}
-  match "tickets/:id", :to => "tickets#update", constraints: {id: /\d+/}, via: "PATCH"
+    put "tickets/:id", :to => "tickets#update", constraints: {id: /\d+/}
+    match "tickets/:id", :to => "tickets#update", constraints: {id: /\d+/}, via: "PATCH"
 
-  post "tickets/:id/tasks", :to => "tasks#create", constraints: {id: /\d+/}
-  put "tasks/:id", :to => "tasks#update", constraints: {id: /\d+/}
-  match "tasks/:id", :to => "tasks#update", constraints: {id: /\d+/}, via: "PATCH"
-  delete "tasks/:id", :to => "tasks#destroy", constraints: {id: /\d+/}
+    post "tickets/:id/tasks", :to => "tasks#create", constraints: {id: /\d+/}
+    put "tasks/:id", :to => "tasks#update", constraints: {id: /\d+/}
+    match "tasks/:id", :to => "tasks#update", constraints: {id: /\d+/}, via: "PATCH"
+    delete "tasks/:id", :to => "tasks#destroy", constraints: {id: /\d+/}
+  end
 
 end
