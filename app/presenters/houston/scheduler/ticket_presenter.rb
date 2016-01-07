ecf5ece01d5761ae59f6940fdcd8c63528e1127a
Bob Lail <bob.lailfamily@gmail.com>
2016-01-06 18:26:23 -0600

    [fix] Stopped presenting sprintId on tickets (1m)

--- app/presenters/houston/scheduler/ticket_presenter.rb
+++ app/presenters/houston/scheduler/ticket_presenter.rb
@@ -16,7 +16,6 @@ class Houston::Scheduler::TicketPresenter < TicketPresenter
         name: reporter.name },
       milestoneId: ticket.milestone_id,
       milestoneName: ticket.milestone.try(:name),
-      sprintId: ticket.sprint_id,
       resolved: ticket.resolved?,
       prerequisites: ticket.prerequisites)
   end
